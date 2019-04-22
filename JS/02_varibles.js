9fee0dc8b08e6f0316958a764cf3e88ae293a8a9
soniandrea <soni291100@gmail.com>
2019-04-22 11:00:28 +0200

    ejercicio variables de JS

---
+++ JS/02_varibles.js
@@ -0,0 +1,47 @@
+//ES6
+
+let nombre //Declarar
+nombre ='Pepe' //Inicializar
+
+let edad = 34 //Declarar e inicializar
+
+//Tipos de datos
+
+//undefined
+let data
+console.log(data)
+console.log(typeof data)
+
+
+// strings
+data = 'Hola amigos'
+console.log(data)
+console.log(typeof data)//tipo de dato que estoy guardando en una variable
+
+
+//numbers
+data = 35
+console.log(data)
+console.log(typeof data)
+
+//booleans
+data = true
+data = false
+console.log(data)
+console.log(typeof data)
+
+//object
+
+data = {name: 'Pepe', age: 34}
+console.log(data)
+console.log(typeof data)
+
+//object tipo array
+data = [23, 45, 78]
+console.log(data)
+console.log(typeof data)
+
+function mostrar () {}
+
+console.log(mostrar)
+console.log(typeof mostrar)
