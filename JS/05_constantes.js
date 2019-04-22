3b3d17ed6ff67c5015fb657c2abc582b989046d9
soniandrea <soni291100@gmail.com>
2019-04-22 13:44:02 +0200

    ejercicio de constantes de JS

---
+++ JS/05_constantes.js
@@ -0,0 +1,32 @@
+let data = 20
+data = 30
+
+console.log(data)
+
+const DATA = 25 // las constantes se ponen en mayúscla
+// DATA = 50 daría un error
+
+console.log(DATA)
+
+const user = {
+    name: 'Pepe',
+    edad: 25
+}
+
+// Java / C# ... son Lenguajes Orientados a Objetos (LOO) basados en clases
+// user1 = new User()
+
+// JS es un LOO basado en prototipos y objetos literales
+
+user.altura = 179
+user.name= 'Carlos'
+
+user = 23
+
+console.log (user)
+console.log (user.name)
+console.log (user.edad)
+
+
+
+
