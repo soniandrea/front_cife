6023b7847183b251a960e8f2c0d45959825608d0
soniandrea <soni291100@gmail.com>
2019-04-22 13:04:35 +0200

    ejercicio sobre ambitos de JS

---
+++ JS/04_ambitos.js
@@ -0,0 +1,52 @@
+// Ambitos o  scope de las variables
+
+// Ambito global
+let var1 = 20
+prueba()
+
+
+// MUY MALA PRÁCTICA
+// No se accede nunca a las variables globales desde las funciones
+function prueba(){
+    console.log(var1)
+}
+
+
+// Lo mismo con BUENAS PRÁCTICAS
+
+let var2 = 30
+
+function pruebaBien (x) {
+    console.log(x)
+}
+
+pruebaBien(var2)
+
+
+// Nuevo en ES6 usar let en lugar de var
+
+
+function condicional(){
+    let ext = 22
+    if(true) {
+        let data = 23
+        console.log(ext)
+        console.log(data)
+    }
+    console.log(ext)
+    // console.log(data) daría un error
+}
+
+condicional()
+
+let x = 12
+
+function otraPrueba () {
+    let x = 23
+    //x = 23
+    console.log('Dentro de la funcion x vale', x)
+}
+
+x=14
+otraPrueba()
+console.log('Fuera de la función x vale', x)
