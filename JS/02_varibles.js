b563de38243c7d79c5b11ab7cfad820222f32362
soniandrea <soni291100@gmail.com>
2019-04-23 09:48:38 +0200

    cambios en ejercicio 02_variables

--- JS/02_varibles.js
+++ JS/02_varibles.js
@@ -1,11 +1,21 @@
-//ES6
+// En ES6 se utiliza let y const para declarar variables
 
 let nombre //Declarar
-nombre ='Pepe' //Inicializar
+nombre = 'Pepe' //Inicializar
 
 let edad = 34 //Declarar e inicializar
 
 //Tipos de datos
+
+/* 
+En otros lenguajes hay declaración EXPLICITA y OBLIGATORIA de tipos
+int x -> variable x de tipo entero sin valor
+
+En JS los tipos son
+ - débiles: no se declaran explícitamente: son IMPLÍCITOS al valor
+ - dinámicos: pueden cambiar
+
+*/
 
 //undefined
 let data
@@ -40,10 +50,11 @@ console.log(typeof data)
 
 //object tipo array
 data = [23, 45, 78]
+data = ['Lunes', 'Martes', 23, 78, true, undefined, {}]
 console.log(data)
 console.log(typeof data)
 
-function mostrar () {}
+function mostrar() {}
 
 console.log(mostrar)
 console.log(typeof mostrar)
