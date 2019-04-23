4868c1af21ee4f58d02bd2d9faf1d052568ec99a
soniandrea <soni291100@gmail.com>
2019-04-23 09:57:42 +0200

    cambios en 02_variables_plus

--- JS/02_varibles_plus.js
+++ JS/02_varibles_plus.js
@@ -11,11 +11,9 @@ let edad = 34 //Declarar e inicializar
 let data
 mostrar(data)
 
-
 // strings
 data = 'Hola amigos'
 mostrar(data)
-
 
 //numbers
 data = 35
@@ -27,12 +25,12 @@ data = false
 mostrar(data)
 
 //object
-
 data = {name: 'Pepe', age: 34}
 mostrar(data)
 
 //object tipo array
 data = [23, 45, 78]
+data = ['Lunes', 'Martes', 23, 78, true, undefined]
 mostrar(data)
 
 function mostrar (input) {
@@ -41,4 +39,13 @@ function mostrar (input) {
     console.log('---------------')
 }
 
+
+/* let  mostrar = function (input) {
+    console.log('El dato recibido es' , input)
+    console.log('El tipo del dato es' , typeof input)
+    console.log('---------------')
+} */
+
 mostrar(mostrar)
+
+let obj = {}
