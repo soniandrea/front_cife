9d9c4ffd8e3b8c7712a657a61779405748617a21
soniandrea <soni291100@gmail.com>
2019-04-29 13:34:02 +0200

    ejercicio 20 sin terminaaaaaaar

---
+++ JS/20_asincrono.js
@@ -0,0 +1,17 @@
+/**
+ * La función setTimeout
+ * permite un proceso asíncrono diferido en el tiempo
+ */
+
+
+ console.log('Primero')
+function paraLuego (){
+    console.log
+}
+
+ // dentro de un rato 
+ // setTimeout (paraLuego, 2000)
+ setTimeout(() => (
+    console.log('Segundo')
+ ), 2000)
+ console.log('Tercero')
